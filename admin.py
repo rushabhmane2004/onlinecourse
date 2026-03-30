df3c64af22bed84fc70e9e75ab29038e3f94b5c9
rushabhmane2004 <rushabhmane2004@gmail.com>
2026-03-30 09:43:16 +0530

    Added admin configurations for Task 2

---
+++ admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('content', 'course', 'grade')
+    list_filter = ['course']
+    search_fields = ['content']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'course']
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
