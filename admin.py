089a458857a22e1d072d1842d55132b8d60cd9c0
rushabhmane2004 <rushabhmane2004@gmail.com>
2026-03-30 13:32:01 +0530

    Django installed and project created

--- admin.py
+++ admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 4
+
 
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ('content', 'course', 'grade')
-    list_filter = ['course']
-    search_fields = ['content']
+    list_display = ("content", "course", "grade")
+    list_filter = ["course"]
+    search_fields = ["content"]
+
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title', 'course']
+    list_display = ["title", "course"]
+
 
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
--- models.py
+++ models.py
@@ -5,15 +5,19 @@ class Question(models.Model):
 
     def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
-        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        selected_correct = self.choice_set.filter(
+            is_correct=True, id__in=selected_ids
+        ).count()
         if all_answers == selected_correct:
             return True
         return False
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
+
 
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
